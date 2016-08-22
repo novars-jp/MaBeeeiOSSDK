5730d6d8d13ee1892199981ef6db5731df7d38a0
tksshj <tksshj@gmail.com>
2016-08-22 16:10:10 +0900

    ScanUISwift追加

---
+++ samples/ScanUISwift/ScanUISwift/ScanTableViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ScanTableViewController.swift
+//  ScanUISwift
+//
+//  Created by Takeshi Shoji on 8/22/16.
+//  Copyright © 2016 Novars Inc. All rights reserved.
+//
+
+import UIKit
+import MaBeeeSDK
+
+class ScanTableViewController: UITableViewController {
+
+  var maBeeeDevices:[MaBeeeDevice]?
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Done,
+                                                            target: self,
+                                                            action: #selector(doneButtonPressed))
+  }
+
+  func doneButtonPressed() {
+    self.dismissViewControllerAnimated(true, completion: nil)
+  }
+
+  override func viewDidAppear(animated: Bool) {
+    super.viewDidAppear(animated)
+    MaBeeeApp.instance().addObserver(self, selector: #selector(receiveNotification(_:)))
+    MaBeeeApp.instance().startScan({(devices: [MaBeeeDevice]!) in
+      self.maBeeeDevices = devices
+      self.tableView.reloadData()
+    })
+  }
+
+  func receiveNotification(notification: NSNotification) {
+    self.tableView.reloadData()
+  }
+
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    if let devices = maBeeeDevices {
+      return devices.count;
+    }
+    return 0;
+  }
+
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("MaBeeeDeviceCell", forIndexPath: indexPath)
+    if let device = maBeeeDevices?[indexPath.row] {
+      cell.textLabel?.text = String(format: "%@, %d, %@",
+                                    device.name,
+                                    device.rssi,
+                                    self.stateString(device.state))
+    }
+    return cell;
+  }
+
+  override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    if let device = maBeeeDevices?[indexPath.row] {
+      switch (device.state) {
+      case .Disconnected:
+        MaBeeeApp.instance().connect(device)
+      case .Connecting:
+        MaBeeeApp.instance().disconnect(device)
+      case .Connected:
+        MaBeeeApp.instance().disconnect(device)
+      }
+    }
+  }
+
+
+  func stateString(state: MaBeeeDeviceState) -> String {
+    var stateString: String
+    switch (state) {
+    case .Disconnected:
+      stateString = "Disconnected"
+    case .Connecting:
+      stateString = "Connecting"
+    case .Connected:
+      stateString = "Connected"
+    }
+    return stateString
+  }
+}
---
+++ samples/ScanUISwift/ScanUISwift/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  ScanUISwift
+//
+//  Created by Takeshi Shoji on 8/22/16.
+//  Copyright © 2016 Novars Inc. All rights reserved.
+//
+
+import UIKit
+import MaBeeeSDK
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+  @IBAction func sliderValueChanged(sender: UISlider) {
+    for device in MaBeeeApp.instance().devices() {
+      device.pwmDuty = Int32(sender.value * 100)
+    }
+  }
+}
+
